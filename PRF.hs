ce01641362ad9b7de1077a99d907124f3c8fb9e6
Constantine Ter-Matevosian <terkot1010@gmail.com>
2020-06-17 17:25:11 +0300

    Added Not + massively cleaned up the code

--- PRF.hs
+++ PRF.hs
@@ -6,13 +6,20 @@ module PRF
     , self
     , one
     , nth
+    , flip2
+    , duplicate
     , prfSum
     , prfProduct
     , prfPower
     , prfLimitedDecrement
     , prfLimitedSub
+    , prfLessOrEquals
     , prfLess
+    , prfEquals
     , prfFactorial
+    , prfIf
+    , prfNot
+    , printPRF
     ) where
 
 data PRF
@@ -53,9 +60,6 @@ compute' (R f g) args
                                 in compute g $ args' ++ [compute (R f g) args']
     | otherwise = error "R: length args == 0"
 
-newline :: IO ()
-newline = putStrLn ""
-
 -- utility functions
 self = P 1 1
 one = C S [Z]
@@ -64,36 +68,78 @@ nth x
     | x < 0 = error $ "nth: argument is less than zero: <" ++ show x ++ ">"
     | x == 0 = Z
     | otherwise = C S [nth (x - 1)]
+flip2 prf = C prf [P 2 2, P 2 1]
+duplicate prf = C prf [self, self]
 
 -- homework functions
 prfSum = R self (C S [P 3 3])
 prfProduct = R Z (C prfSum [P 3 1, P 3 3])
 prfPower = R one (C prfProduct [P 3 1, P 3 3])
-prfLimitedDecrement = C (R Z (P 3 2)) [self, self]
+prfLimitedDecrement = duplicate $ R Z (P 3 2)
 prfLimitedSub = R self (C prfLimitedDecrement [P 3 3])
-prfFalseLess = C (R one (C Z [P 3 1])) [P 2 1, prfLimitedSub] -- incorrect
+prfLessOrEquals = C (R one (C Z [P 3 1])) [P 2 1, prfLimitedSub]
 prfLess = C (R one (C Z [P 3 1])) [P 2 1, C prfLimitedSub [C S [P 2 1], P 2 2]]
-prfFactorial = C (R one (C prfProduct [C S [P 3 2], P 3 3])) [self, self]
+prfEquals = C prfProduct [prfLessOrEquals, flip2 prfLessOrEquals]
+prfFactorial = duplicate $ R one (C prfProduct [C S [P 3 2], P 3 3])
+prfIf = R (P 2 2) (P 4 1)
+prfNot = duplicate $ R one (C Z [P 3 1])
 
-printPRF :: PRF -> [Integer] -> IO ()
-printPRF prf args = print $ prf `compute` args
+newline :: IO ()
+newline = putStrLn ""
+
+listArgs :: [Integer] -> String
+listArgs args = "(" ++ ((init . tail . show) args) ++ ")"
+
+printPRF :: PRF -> String -> [Integer] -> IO ()
+printPRF prf name args
+    = putStrLn
+    $ name ++ (listArgs args) ++ " = " ++ show (prf `compute` args)
 
 main :: IO ()
 main = do
-    printPRF prfSum [12, 13]
-    printPRF prfProduct [12, 13]
-    printPRF prfPower [3, 5]
-    printPRF prfLimitedDecrement [4]
-    printPRF prfLimitedDecrement [0]
-    printPRF prfLimitedSub [8, 6]
-    printPRF prfLimitedSub [8, 8]
-    printPRF prfLimitedSub [6, 6]
-    printPRF prfFalseLess [43, 12]
-    printPRF prfFalseLess [12, 12]
-    printPRF prfFalseLess [11, 12]
-    printPRF prfLess [43, 12]
-    printPRF prfLess [12, 12]
-    printPRF prfLess [11, 12]
-    printPRF prfFactorial [0]
-    printPRF prfFactorial [1]
-    printPRF prfFactorial [7]
+    putStrLn $ "=== Sum ==="
+    printPRF prfSum "Sum" [12, 13]
+    printPRF prfSum "Sum" [0, 2]
+    printPRF prfSum "Sum" [2, 0]
+    printPRF prfSum "Sum" [0, 0]
+    putStrLn $ "=== Product ==="
+    printPRF prfProduct "Product" [12, 13]
+    printPRF prfProduct "Product" [0, 2]
+    printPRF prfProduct "Product" [2, 0]
+    printPRF prfProduct "Product" [0, 0]
+    putStrLn $ "=== Power ==="
+    printPRF prfPower "Power" [3, 6]
+    printPRF prfPower "Power" [6, 3]
+    putStrLn $ "=== LimitedDecrement ==="
+    printPRF prfLimitedDecrement "LimitedDecrement" [134]
+    printPRF prfLimitedDecrement "LimitedDecrement" [12]
+    printPRF prfLimitedDecrement "LimitedDecrement" [1]
+    printPRF prfLimitedDecrement "LimitedDecrement" [0]
+    putStrLn $ "=== LimitedSub ==="
+    printPRF prfLimitedSub "LimitedSub" [8, 6]
+    printPRF prfLimitedSub "LimitedSub" [8, 8]
+    printPRF prfLimitedSub "LimitedSub" [6, 6]
+    putStrLn $ "=== LessOrEquals ==="
+    printPRF prfLessOrEquals "LessOrEquals" [43, 12]
+    printPRF prfLessOrEquals "LessOrEquals" [12, 12]
+    printPRF prfLessOrEquals "LessOrEquals" [11, 12]
+    putStrLn $ "=== Less ==="
+    printPRF prfLess "Less" [43, 12]
+    printPRF prfLess "Less" [12, 12]
+    printPRF prfLess "Less" [11, 12]
+    putStrLn $ "=== Equals ==="
+    printPRF prfEquals "Equals" [23, 41]
+    printPRF prfEquals "Equals" [21, 43]
+    printPRF prfEquals "Equals" [32, 32]
+    putStrLn $ "=== Factorial ==="
+    printPRF prfFactorial "Factorial" [0]
+    printPRF prfFactorial "Factorial" [1]
+    printPRF prfFactorial "Factorial" [6]
+    putStrLn $ "=== If ==="
+    printPRF prfIf "If" [3, 4, 0]
+    printPRF prfIf "If" [3, 4, 1]
+    printPRF prfIf "If" [3, 4, 1111]
+    putStrLn $ "=== Not ==="
+    printPRF prfNot "Not" [23]
+    printPRF prfNot "Not" [1]
+    printPRF prfNot "Not" [0]
--- main.cpp
+++ main.cpp
@@ -10,6 +10,12 @@ using NatArgs = std::vector<unsigned>;
 using Id = U<1, 1>;
 using One = S<N, Z>;
 
+template<typename T>
+using Flip2 = S<T, U<2, 2>, U<2, 1>>;
+
+template<typename T>
+using Duplicate = S<T, Id, Id>;
+
 using Sum = R<Id, S<N, U<3, 3>>>;
 /*
  * R<U<1, 1>, S<N, U<3, 3>>>(4, 2)
@@ -39,7 +45,7 @@ using Product = R<Z, S<Sum, U<3, 1>, U<3, 3>>>;
  * 8
  */
 
-using LimitedDecrement = S<R<Z, U<3, 2>>, Id, Id>;
+using LimitedDecrement = Duplicate<R<Z, U<3, 2>>>;
 /*
  * S<R<Z, U<3, 2>>, Id, Id>(3)
  * R<Z, U<3, 2>>(Id(3), Id(3))
@@ -73,30 +79,57 @@ using LimitedSub = R<Id, S<LimitedDecrement, U<3, 3>>>;
  * the case when a < b is analogous
  */
 
-using LessOrEquals = S<R<One, S<Z, U<3, 1>>>, U<2, 1>, LimitedSub>;//:NEW new name
+using LessOrEquals = S<R<One, S<Z, U<3, 1>>>, U<2, 1>, LimitedSub>;
 /*
  * S<R<One, S<Z, U<3, 1>>>, U<2, 1>, LimitedSub>(1, 2)
  * R<One, S<Z, U<3, 1>>>(U<2, 1>(1, 2), LimitedSub(1, 2))
  * R<One, S<Z, U<3, 1>>>(1, 0)
  * S<One, Id>(1)
- * 1 - True
+ * 1
  *
  * S<R<One, S<Z, U<3, 1>>>, U<2, 1>, LimitedSub>(2, 2)
  * R<One, S<Z, U<3, 1>>>(2, 0)
  * One(2)
- * 1 - False
+ * 1
  *
  * S<R<One, S<Z, U<3, 1>>>, U<2, 1>, LimitedSub>(2, 1)
  * R<One, S<Z, U<3, 1>>>(2, 1)
  * S<Z, U<3, 1>>(2, 0, R<One, S<Z, U<3, 1>>>(2, 0))
  * S<Z, U<3, 1>>(2, 0, 1)
- * 0 - True
+ * 0
  */
-using Less = S<R<One, S<Z, U<3, 1>>>, U<2, 1>, S<LimitedSub, S<N, U<2, 1>>, U<2, 2>>>;
 
-using Equals = S<Product, S<LessOrEquals, U<2, 1>, U<2, 2>>, S<LessOrEquals, U<2, 2>, U<2, 1>>>;//:NEW
+using Less = S<LessOrEquals, S<N, U<2, 1>>, U<2, 2>>;
+/*
+ * S<LessOrEquals, S<N, U<2, 1>>, U<2, 2>>(1, 2)
+ * LessOrEquals(2, 2)
+ * 1
+ *
+ * S<LessOrEquals, S<N, U<2, 1>>, U<2, 2>>(2, 2)
+ * LessOrEquals(3, 2)
+ * 0
+ *
+ * S<LessOrEquals, S<N, U<2, 1>>, U<2, 2>>(2, 1)
+ * LessOrEquals(3, 1)
+ * 0
+ */
 
-using Factorial = S<R<One, S<Product, S<N, U<3, 2>>, U<3, 3>>>, Id, Id>;
+using Equals = S<Product, LessOrEquals, Flip2<LessOrEquals>>;
+/*
+ * S<Product, LessOrEquals, S<LessOrEquals, U<2, 2>, U<2, 1>>>(4, 6)
+ * Product(LessOrEquals(4, 6), S<LessOrEquals, U<2, 2>, U<2, 1>>(4, 6))
+ * Product(LessOrEquals(4, 6), LessOrEquals(6, 4))
+ * Product(1, 0)
+ * 0
+ *
+ * S<Product, LessOrEquals, S<LessOrEquals, U<2, 2>, U<2, 1>>>(5, 5)
+ * Product(LessOrEquals(5, 5), S<LessOrEquals, U<2, 2>, U<2, 1>>(5, 5))
+ * Product(LessOrEquals(5, 5), LessOrEquals(5, 5))
+ * Product(1, 1)
+ * 1
+ */
+
+using Factorial = Duplicate<R<One, S<Product, S<N, U<3, 2>>, U<3, 3>>>>;
 /*
  * S<R<One, S<Product, S<N, U<3, 2>>, U<3, 3>>>, Id, Id>(3)
  * R<One, S<Product, S<N, U<3, 2>>, U<3, 3>>>(3, 3)
@@ -115,8 +148,40 @@ using Factorial = S<R<One, S<Product, S<N, U<3, 2>>, U<3, 3>>>, Id, Id>;
  * 6
  */
 
-using If = S<R<U<2, 2>, U<4, 1>>, U<3, 2>, U<3, 3>, U<3, 1>>;//:NEW - возвращает третий аргумент, если первый аргумент равен нулю, иначе-возвращает второй аргумент
-using DivMax = Z;//:NEW
+using If = R<U<2, 2>, U<4, 1>>;
+/*
+ * returns the second argument if the third one is 0, otherwise returns the first argument
+ *
+ * R<U<2, 2>, U<4, 1>>(3, 2, 0)
+ * U<2, 2>(3, 2)
+ * 2
+ *
+ * R<U<2, 2>, U<4, 1>>(3, 2, 1)
+ * U<4, 1>(3, 2, 0, R<U<2, 2>, U<4, 1>>(3, 2, 0))
+ * U<4, 1>(3, 2, 0, U<2, 2>(3, 2))
+ * U<4, 1>(3, 2, 0, 2)
+ * 3
+ */
+
+using Not = Duplicate<R<One, S<Z, U<3, 1>>>>;
+/*
+ * returns 0 if the argument is non-zero, otherwise returns 1
+ *
+ * S<R<One, S<Z, U<3, 1>>>, Id, Id>(2)
+ * R<One, S<Z, U<3, 1>>>(2, 2)
+ * S<Z, U<3, 1>>(2, 1, R<One, S<Z, U<3, 1>>>(2, 1))
+ * S<Z, U<3, 1>>(2, 1, S<Z, U<3, 1>>(2, 0, R<One, S<Z, U<3, 1>>>(2, 0)))
+ * S<Z, U<3, 1>>(2, 1, S<Z, U<3, 1>>(2, 0, One(2)))
+ * S<Z, U<3, 1>>(2, 1, S<Z, U<3, 1>>(2, 0, 2))
+ * S<Z, U<3, 1>>(2, 1, 0)
+ * 0
+ *
+ * S<R<One, S<Z, U<3, 1>>>, Id, Id>(0)
+ * One(0)
+ * 1
+ */
+
+using DivMax = Z; // TODO
 using Div = S<R<Id, Id>, U<2, 1>, U<2, 2>, Z, U<2, 1>>; // TODO
 using Mod = Z; // TODO
 using BitAnd = Z; // TODO
@@ -127,28 +192,71 @@ using Pair = Z; // TODO
 using IsPrime = Z; // TODO
 using NthPrime = Z; // TODO
 
+std::string listArgs(const NatArgs& args) {
+    std::string result = "(";
+    for (unsigned arg : args) {
+        result.append(std::to_string(arg));
+        result.append(", ");
+    }
+    if (!args.empty()) {
+        result.std::string::erase(result.end() - 2, result.end());
+    }
+    result.append(")");
+    return result;
+}
+
 template<typename P>
-void printPRF([[maybe_unused]] P prf, const NatArgs &args) {
-    std::cout << P::compute(args) << "\n";
+void printPRF([[maybe_unused]] P prf, const std::string& prfName, const NatArgs& args) {
+    std::cout << prfName << listArgs(args) << " = " << P::compute(args) << "\n";
 }
 
 int main() {
-//    printPRF(Sum(), NatArgs{13, 12});
-//    printPRF(Product(), NatArgs{13, 12});
-//    printPRF(Power(), NatArgs{3, 6});
-//    printPRF(LimitedDecrement(), NatArgs{134});
-//    printPRF(LimitedDecrement(), NatArgs{12});
-//    printPRF(LimitedDecrement(), NatArgs{1});
-//    printPRF(LimitedDecrement(), NatArgs{0});
-//    printPRF(LimitedSub(), NatArgs{8, 6});
-//    printPRF(LimitedSub(), NatArgs{8, 8});
-//    printPRF(LimitedSub(), NatArgs{6, 8});
-//    printPRF(Less(), NatArgs{43, 12});
-//    printPRF(Less(), NatArgs{12, 12});
-//    printPRF(Less(), NatArgs{11, 12});
-//    printPRF(Factorial(), NatArgs{0});
-//    printPRF(Factorial(), NatArgs{1});
-//    printPRF(Factorial(), NatArgs{7});
-    printPRF(If(), NatArgs{0, 3, 4});
+    std::cout << "=== Sum ===" << "\n";
+    printPRF(Sum(), "Sum", NatArgs{13, 12});
+    printPRF(Sum(), "Sum", NatArgs{0, 2});
+    printPRF(Sum(), "Sum", NatArgs{2, 0});
+    printPRF(Sum(), "Sum", NatArgs{0, 0});
+    std::cout << "=== Product ===" << "\n";
+    printPRF(Product(), "Product", NatArgs{13, 12});
+    printPRF(Product(), "Product", NatArgs{2, 0});
+    printPRF(Product(), "Product", NatArgs{0, 2});
+    printPRF(Product(), "Product", NatArgs{0, 0});
+    std::cout << "=== Power ===" << "\n";
+    printPRF(Power(), "Power", NatArgs{3, 6});
+    printPRF(Power(), "Power", NatArgs{6, 3});
+    std::cout << "=== LimitedDecrement ===" << std::endl;
+    printPRF(LimitedDecrement(), "LimitedDecrement", NatArgs{134});
+    printPRF(LimitedDecrement(), "LimitedDecrement", NatArgs{12});
+    printPRF(LimitedDecrement(), "LimitedDecrement", NatArgs{1});
+    printPRF(LimitedDecrement(), "LimitedDecrement", NatArgs{0});
+    std::cout << "=== LimitedSub ===" << std::endl;
+    printPRF(LimitedSub(), "LimitedSub", NatArgs{8, 6});
+    printPRF(LimitedSub(), "LimitedSub", NatArgs{8, 8});
+    printPRF(LimitedSub(), "LimitedSub", NatArgs{6, 8});
+    std::cout << "=== LessOrEquals ===" << std::endl;
+    printPRF(LessOrEquals(), "LessOrEquals", NatArgs{43, 12});
+    printPRF(LessOrEquals(), "LessOrEquals", NatArgs{12, 12});
+    printPRF(LessOrEquals(), "LessOrEquals", NatArgs{11, 12});
+    std::cout << "=== Less ===" << std::endl;
+    printPRF(Less(), "Less", NatArgs{43, 12});
+    printPRF(Less(), "Less", NatArgs{12, 12});
+    printPRF(Less(), "Less", NatArgs{11, 12});
+    std::cout << "=== Equals ===" << "\n";
+    printPRF(Equals(), "Equals", NatArgs{23, 41});
+    printPRF(Equals(), "Equals", NatArgs{43, 21});
+    printPRF(Equals(), "Equals", NatArgs{32, 32});
+    std::cout << "=== Factorial ===" << std::endl;
+    printPRF(Factorial(), "Factorial", NatArgs{0});
+    printPRF(Factorial(), "Factorial", NatArgs{1});
+    printPRF(Factorial(), "Factorial", NatArgs{6});
+    std::cout << "=== If ===" << std::endl;
+    printPRF(If(), "If", NatArgs{3, 4, 0});
+    printPRF(If(), "If", NatArgs{3, 4, 1});
+    printPRF(If(), "If", NatArgs{3, 4, 1111});
+    std::cout << "=== Not ===" << std::endl;
+    printPRF(Not(), "Not", NatArgs{23});
+    printPRF(Not(), "Not", NatArgs{1});
+    printPRF(Not(), "Not", NatArgs{0});
+
     return 0;
 }
